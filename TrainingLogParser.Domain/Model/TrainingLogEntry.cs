9f78f457fd1c659db0987cacc94b3267d81540fb
Tim Collins <tim@timcollins.ie>
2024-09-30 21:26:10 +0100

    Add TrainingLogEntry domain model.

---
+++ TrainingLogParser.Domain/Model/TrainingLogEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace TrainingLogParser.Domain.Model
+{
+    public class TrainingLogEntry
+    {
+        public DateTimeOffset Date { get; set; }
+        public string Exercise { get; set; }
+        public decimal Weight { get; set; }
+        public int Reps { get; set; }
+        public string Notes { get; set; }
+    }
+}
