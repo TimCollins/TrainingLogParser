23b4b673632c90556107ad522ddd5f0d26c7a509
Tim Collins <tim@timcollins.ie>
2024-10-01 21:25:00 +0100

    Move custom date converter to separate class.

---
+++ src/TrainingLogParser.Logic/Converters/CustomDateTimeOffsetConverter.cs
@@ -0,0 +1,22 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using CsvHelper;
+using System.Globalization;
+
+namespace TrainingLogParser.Logic.Converters
+{
+    internal class CustomDateTimeOffsetConverter : DefaultTypeConverter
+    {
+        private const string DateFormat = "dd/MM/yyyy";
+
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            if (DateTimeOffset.TryParseExact(text, DateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out var result))
+            {
+                return result;
+            }
+
+            return base.ConvertFromString(text, row, memberMapData);
+        }
+    }
+}
