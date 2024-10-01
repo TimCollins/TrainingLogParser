ea974d4bd67f8c44d77a7cfd0253cc5beef331dc
Tim Collins <tim@timcollins.ie>
2024-10-01 15:44:14 +0100

    Add TestFixture.

---
+++ src/TrainingLogParser.Tests/Infrastructure/TestFixture.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleInjector;
+using TrainingLogParser.Logic.Command;
+
+namespace TrainingLogParser.Tests.Infrastructure
+{
+    public class TestFixture
+    {
+        public Container Container { get; private set; }
+        public ServiceProvider ServiceProvider { get; }
+
+        public TestFixture()
+        {
+            var services = new ServiceCollection();
+            services.AddMediatR(typeof(ParseTrainingLogCommand));
+
+            ServiceProvider = services.BuildServiceProvider();
+
+            Container = new Container();
+            Container.Options.ResolveUnregisteredConcreteTypes = true;
+
+            Container.Register(() => new ServiceFactory(Container.GetInstance), Lifestyle.Singleton);
+
+            Container.RegisterSingleton<IMediator, Mediator>();
+
+            Container.Verify();
+        }
+    }
+}
