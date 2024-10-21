d3ea4fe428cf6fd6f1be2d3cf241e9a3a3c1c2dc
Tim Collins <tim@timcollins.ie>
2024-10-21 21:42:36 +0100

    Add query to retrieve heaviest bench.

---
+++ src/TrainingLogParser.Logic/Query/GetHeaviestBenchPressQuery.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using TrainingLogParser.Domain.Model;
+using TrainingLogParser.Repo.Interfaces;
+
+namespace TrainingLogParser.Logic.Query
+{
+    public class GetHeaviestBenchPressQuery : IRequest<TrainingLogEntry>
+    {
+    }
+
+    public class GetHeaviestBenchPressHandler : IRequestHandler<GetHeaviestBenchPressQuery, TrainingLogEntry>
+    {
+        private readonly ITrainingLogEntryRepo _trainingLogEntryRepo;
+
+        public GetHeaviestBenchPressHandler(ITrainingLogEntryRepo trainingLogEntryRepo)
+        {
+            _trainingLogEntryRepo = trainingLogEntryRepo;
+        }
+
+        public async Task<TrainingLogEntry> Handle(GetHeaviestBenchPressQuery request, CancellationToken cancellationToken)
+        {
+            var res = await _trainingLogEntryRepo.GetHeaviestSetForExercise("Bench Press");
+
+            return res;
+        }
+    }
+}
