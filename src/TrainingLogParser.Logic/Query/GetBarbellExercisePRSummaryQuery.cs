d6e1d5a5b4e75d03d54bf0d7c03c26f2da3cf4c0
Tim Collins <tim@timcollins.ie>
2024-11-16 16:08:17 +0000

    Add query to get PR summary.

---
+++ src/TrainingLogParser.Logic/Query/GetBarbellExercisePRSummaryQuery.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using TrainingLogParser.Domain.Model;
+using TrainingLogParser.Repo.Interfaces;
+
+namespace TrainingLogParser.Logic.Query
+{
+    public class GetBarbellExercisePRSummaryQuery : IRequest<IEnumerable<TrainingLogEntry>>
+    {
+    }
+
+    public class GetBarbellExercisePRSummaryHandler : IRequestHandler<GetBarbellExercisePRSummaryQuery, IEnumerable<TrainingLogEntry>>
+    {
+        private readonly ITrainingLogEntryRepo _trainingLogEntryRepo;
+
+        public GetBarbellExercisePRSummaryHandler(ITrainingLogEntryRepo trainingLogEntryRepo)
+        {
+            _trainingLogEntryRepo = trainingLogEntryRepo;
+        }
+
+        public async Task<IEnumerable<TrainingLogEntry>> Handle(GetBarbellExercisePRSummaryQuery request, CancellationToken cancellationToken)
+        {
+            var res = await _trainingLogEntryRepo.GetBarbellExercisePRSummaryQuery();
+
+            return res;
+        }
+    }
+}
