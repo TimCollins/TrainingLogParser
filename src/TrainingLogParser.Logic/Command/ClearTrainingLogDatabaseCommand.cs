ee9a31d69c201a995a4c19f528d87dccb698dd16
Tim Collins <tim@timcollins.ie>
2025-01-01 11:59:54 +0000

    Add new command to clear database.

---
+++ src/TrainingLogParser.Logic/Command/ClearTrainingLogDatabaseCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using TrainingLogParser.Repo.Interfaces;
+
+namespace TrainingLogParser.Logic.Command
+{
+    public class ClearTrainingLogDatabaseCommand : IRequest<Unit>
+    {
+    }
+
+    public class ClearTrainingLogDatabaseHandler : IRequestHandler<ClearTrainingLogDatabaseCommand, Unit>
+    {
+        private readonly ITrainingLogEntryRepo _trainingLogEntryRepo;
+
+        public ClearTrainingLogDatabaseHandler(ITrainingLogEntryRepo trainingLogEntryRepo)
+        {
+            _trainingLogEntryRepo = trainingLogEntryRepo;
+        }
+
+        public async Task<Unit> Handle(ClearTrainingLogDatabaseCommand request, CancellationToken cancellationToken)
+        {
+            await _trainingLogEntryRepo.DeleteAllEntries();
+
+            return Unit.Value;
+        }
+    }
+}
