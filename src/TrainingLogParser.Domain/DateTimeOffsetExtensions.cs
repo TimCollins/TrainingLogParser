18d23da8ce8986a2dc4c6ed0f8e287f88a4ebc2d
Tim Collins <tim@timcollins.ie>
2025-01-01 12:04:55 +0000

    Add extension method for date parsing.

---
+++ src/TrainingLogParser.Domain/DateTimeOffsetExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace TrainingLogParser.Domain
+{
+    public static class DateTimeOffsetExtensions
+    {
+        public static string ToSqliteDateFormat(this DateTimeOffset dateTimeOffset)
+        {
+            return dateTimeOffset.ToString(TrainingLogParserConstants.IsoDateFormat);
+        }
+    }
+}
